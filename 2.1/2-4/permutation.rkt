1ed2f85d037309ebbd01b662d565b8728b946a04
ghking1 <ghking6@gmail.com>
2017-01-14 10:59:59 +0800

    add comment and test to code in 2.1

--- 2.1/2-4/permutation.rkt
+++ 2.1/2-4/permutation.rkt
@@ -1,52 +1,58 @@
 #lang racket
 
-(define (permutate list k m)
-  (let [(permutations '()) (count 0)]
-    (define (permutate_inner list k m)
-      (if (= k m)
-          (set! permutations (cons list permutations))
-          ((lambda ()
+(define (permutate list)
+  (let [(permutations '())] ;permutations used to save all list of permutation
+    (define (permutate_inner list start end) ;start and end position for permutate
+      (if (= start end)
+          (set! permutations (cons list permutations)) ;when recursive to the last element, then the list is a new permutation, cons it to permutations
+          ((lambda () ;this lambda and define realized the action like named-let
              (define (loop i)
-               (if (< i m)
-                   (begin (permutate_inner (swap list k i) (+ k 1) m)
-                          (loop (+ i 1))
+               (if (< i end)
+                   (begin (permutate_inner (swap list start i) (+ start 1) end) ;permutate from strat+1 to end after swap i-th element to the start position
+                          (loop (+ i 1)) ;it will generate next serials permutation which use (i+1)-th element as first element
                           )
                    (void)
                    )
                )
-             (loop k)
+             (loop start) ;begain loop from start position
              ))
           )
       )
-    (permutate_inner list k m)
+    (permutate_inner list 0 (length list))
     permutations
     )
   )
 
-(define (swap list i j)
+(define (swap list i j)  ;swap two element in list, it return a new list
   (let [(a '()) (b '())]
     (define (swap_inner list i j fore middle n count)
       (cond
-        [(= i j) list]
-        [(= count i) (set! a (cons (car list) a))
-                     (swap_inner (cdr list) i j fore middle n (+ count 1))
-                     ]
-        [(= count j) (set! b (cons (car list) b))
-                     (swap_inner (cdr list) i j fore middle n (+ count 1))
-                     ]
-        [(< count i) (swap_inner (cdr list) i j
+        [(= i j) list] ;if i==j, there is no need to swap
+        [(< count i) (swap_inner (cdr list) i j ;when count<i cons the count-th element to fore
                                  (cons (car list) fore) middle
                                  n (+ count 1)
                                  )
                      ]
-        [(and (> count i) (< count j)) (swap_inner (cdr list) i j
+        [(= count i) (set! a (cons (car list) a)) ;when count==i set the count-th element to a
+                     (swap_inner (cdr list) i j fore middle n (+ count 1))
+                     ]
+        [(and (> count i) (< count j)) (swap_inner (cdr list) i j ;when i<count<j, cons the count-th element to middle
                                                    fore (cons (car list) middle)
                                                    n (+ count 1)
                                                    )
                                        ]
-        [(or (empty? list) (> count j)) (append (reverse fore) b (reverse middle) a list)]
+        [(= count j) (set! b (cons (car list) b)) ;when count==j set the count-th element to b
+                     (swap_inner (cdr list) i j fore middle n (+ count 1))
+                     ]
+        [(or (empty? list) (> count j)) (append (reverse fore) b (reverse middle) a list)] ;when count>j, join fore b middle a remaind-list as final list. but fore and middle should be reversed
         )
       )
     (swap_inner list i j '() '() (length list) 0)
     )
   )
+
+
+;******************************************************test part**************************************************************
+
+(permutate '(1 2 3))
+(permutate '(1 2 3 4 5))
--- 2.1/2-5/integer_division.rkt
+++ 2.1/2-5/integer_division.rkt
@@ -1,17 +1,27 @@
 #lang racket
 
-(define (integer_division n m)
-  (if (or (< n 1) (< m 1))
-      0
-      (if (or (= n 1) (= m 1))
-          1
-          (if (< n m)
-              (q n n)
-              (if (= n m)
-                  (+ 1 (q n (- m 1)))
-                  (+ (q n (- m 1)) (q (- n m) m))
-                  )
-              )
-          )
-      )
+(define (integer_division n)
+  (define (integer_division_inner n m) ;the division elements of n is not bigger then m, return value is the kinds of division
+    (if (or (< n 1) (< m 1))
+        0
+        (if (or (= n 1) (= m 1))
+            1
+            (if (< n m)
+                (integer_division_inner n n)
+                (if (= n m)
+                    (+ 1 (integer_division_inner n (- m 1)))
+                    (+ (integer_division_inner n (- m 1)) (integer_division_inner (- n m) m))
+                    )
+                )
+            )
+        )
+    )
+  (integer_division_inner n n)
   )
+
+
+;******************************************************test part**************************************************************
+(integer_division 1)
+(integer_division 3)
+(integer_division 5)
+(integer_division 10)
